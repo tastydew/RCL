b167aeb5b81dd6171f892e59501ca17316162a0a
Fredy Teyssedou <teyssedou_f@subway.com>
2018-02-26 16:23:54 -0500

    Added NotFound Exception, updated to RC of SpringBoot, Added more methods to controller'

--- src/main/java/rcl/controller/ContactController.java
+++ src/main/java/rcl/controller/ContactController.java
@@ -1,14 +1,23 @@
 package rcl.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Example;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import rcl.controller.exceptions.NotFoundException;
 import rcl.model.Contact;
 import rcl.repository.ContactRepository;
 
@@ -20,39 +29,55 @@ public class ContactController {
 	private ContactRepository contactRepo;
 
 	@GetMapping("/contacts/{id}")
-	public ResponseEntity<Contact> contact(@PathVariable("id") Long contactId) {
-		return ResponseEntity.ok(contactRepo.findOne(contactId));
+	public Optional<Contact> contact(@PathVariable("id") Long contactId) {
+		return contactRepo.findById(contactId);
 
 	}
-	
-	@GetMapping("/contacts")
-	public List<Contact> allContacts(){
-		return contactRepo.findAll();
+
+	 @GetMapping("/contacts")
+	 public List<Contact> allContacts() {
+	 return contactRepo.findAll();
+	 }
+
+	@PostMapping("/contacts")
+	@ResponseStatus(HttpStatus.CREATED)
+	public List<Contact> addContacts(@RequestBody List<Contact> newContact) {
+		return contactRepo.saveAll(newContact);
 	}
-	
-	@GetMapping("/addContacts")
-	public String addContacts() {
+
+	@GetMapping(value = "/contacts")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Contact> getContactByFirstName(@RequestParam("firstName") String name,
+			@RequestParam("lastName") String lastName) {
+
+		Contact contact = new Contact().setFirstName(name).setLastName(lastName);
+		Example<Contact> ex = Example.of(contact);
+		return contactRepo.findAll(ex);
+	}
+
+	@DeleteMapping("/contacts/{id}")
+	public void deleteContact(@PathVariable long id) {
+		contactRepo.deleteById(id);
+	}
+
+	@PutMapping("/contacts/{id}")
+	public Contact updateContact(@PathVariable long id, @RequestBody Contact contact) throws NotFoundException {
+
+		Optional<Contact> contactId = contactRepo.findById(id);
+		if (contactId.isPresent()) {
+			Contact updatedContact = contactId.get();
+			updatedContact.setNameTitle(contact.getNameTitle()).setFirstName(contact.getFirstName())
+					.setMiddleInitial(contact.getMiddleInitial()).setLastName(contact.getLastName())
+					.setCompanyName(contact.getCompanyName()).setAddressOne(contact.getAddressOne())
+					.setAddressTwo(contact.getAddressTwo()).setCityName(contact.getCityName())
+					.setStateName(contact.getStateName()).setZipCode(contact.getZipCode())
+					.setCountry(contact.getCountry()).setPhoneNumber(contact.getPhoneNumber());
+			return contactRepo.save(updatedContact);
+		}
 		
-		Contact testContact = new Contact()
-		.setNameTitle("Mr.")
-		.setFirstName("Fredy")
-		.setMiddleInitial('A')
-		.setLastName("Teyssedou")
-		.setCompanyName("Subway")
-		.setAddressOne("310 Dyer St.")
-		.setAddressTwo("APT 3L")
-		.setCityName("New Haven")
-		.setStateName("CT")
-		.setZipCode(06515)
-		.setCountry("United States")
-		.setPhoneNumber(2035359518);
+		throw new NotFoundException("Could not find contact with ID: " + id);
 		
-		
-		contactRepo.save(testContact);
-		
-		
-		return "ok";
+
 	}
 
 }
-
--- src/main/java/rcl/repository/ContactRepository.java
+++ src/main/java/rcl/repository/ContactRepository.java
@@ -1,5 +1,7 @@
 package rcl.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,10 @@ import rcl.model.Contact;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+	
+	List<Contact> findByFirstName(String name);
+	List<Contact> findByLastName(String name);
+	List<Contact> findByAddressOne(String address);
+	
 	
 }
